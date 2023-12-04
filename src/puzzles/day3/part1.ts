5cd678481dfc0d57e5fa3119a5dee69e4d9ac657
dsaladbar617 <dsalazar219@gmail.com>
2023-12-04 14:26:19 -0600

    working on getting numbers seperated

--- src/puzzles/day3/part1.ts
+++ src/puzzles/day3/part1.ts
@@ -21,7 +21,6 @@ const getAllNumAndSpecCharIndex = (matrix: string[][]): numCharMatrix => {
 };
 
 const padMatrix = (matrix: numCharMatrix): numCharMatrix => {
-  // console.log(toInsert)
   matrix.forEach((item) => {
     item.unshift('.')
     item.push('.')
@@ -36,27 +35,81 @@ const sorted = getAllNumAndSpecCharIndex(matrix)
 
 const paddedMatrix = padMatrix(sorted);
 
-// console.log(paddedMatrix.join('\n'))
+const getAdjacentSpaces = (i: number, j: number) => {
+      let topL = paddedMatrix[i-1][j-1]
+      let topC = paddedMatrix[i-1][j]
+      let topR = paddedMatrix[i-1][j+1]
+      let left = paddedMatrix[i][j-1]
+      let center = paddedMatrix[i][j]
+      let right = paddedMatrix[i][j+1]
+      let bottomL = paddedMatrix[i+1][j-1]
+      let bottomC = paddedMatrix[i+1][j]
+      let bottomR = paddedMatrix[i+1][j+1]
 
-const checkAdjacent = ( matrix: numCharMatrix) => {
-  let partNumArr = []
+      return {topL, topC, topR, left, center, right, bottomL, bottomC, bottomR}
+}
 
-  for (let i = 1; i < matrix.length - 1; i++) {
-    for (let j = 1; j < matrix[i].length - 1; j++) {
-      console.log('no match', i,j, matrix[i][j])
-      if (matrix[i][j] === 1 && matrix[i-1][j-1] === 1 || matrix[i+1][j+1] === 1) {
-        console.log('match', i, j, matrix[i][j])
-        partNumArr.push([i,j])
-      }
+const getNum = (arr) => {
+  let indexes = []
+  let numIndex = []
+  arr.forEach( (elem, index) => {
+    if (elem === 0) numIndex.push(index) // indexes.push(index)
+    if (elem !== 0 && numIndex.length) {
+      indexes.push(numIndex)
+      numIndex = []
     }
+  })
+  return indexes
+  // return arr.filter((item, lineIndex) => item === 0).map((item, index) => index )
 }
-console.log(partNumArr)
+
+const checkAdjacent = ( matrix: string[][]) => {
+
+  const sorted = getAllNumAndSpecCharIndex(matrix)
+
+  const paddedMatrix = padMatrix(sorted);
+
+  let partNumArr = []
+  let numArr = []
+  let numPaddedIndex = []
+  let end = []
+
+  for (let i = 1; i < paddedMatrix.length - 1; i++) {
+    let num = ''
+    let start = []
+    console.log(getNum(paddedMatrix[i]))
+    for (let j = 1; j < paddedMatrix[i].length - 1; j++) {
+      const adjacentSpaces = getAdjacentSpaces(i,j)
+
+
+      if (adjacentSpaces.center === 0) {
+        start.push([i,j])
+        if (adjacentSpaces.right === 0) {
+          num += matrix[i - 1][j - 1]
+          numPaddedIndex.push([i, j])
+        }
+        else if (adjacentSpaces.right !== 0) {
+          // end.push([i,j])
+          if (start.length > 0 )end.push(start)
+          num += matrix[i - 1][j - 1]
+          numPaddedIndex.push([i, j])
+          if( Object.values(adjacentSpaces).includes(1) ) partNumArr.push(num)
+          numArr.push(num)
+        num = ''
+      }
+      // if( adjacentSpaces.includes(1)) partNumArr.push(num)
+      }
+  }
+  // console.log(end)
 }
-// console.log(matrix[3 - 1][4 - 1])
 
-checkAdjacent(paddedMatrix)
+// console.log(numArr)
+// console.log(partNumArr)
+// console.log(numPaddedIndex)
+// console.log(numArr.reduce((total, item) => total += +item, 0 ))
+}
 
-// fs.writeFileSync(path.join(__dirname, 'check.txt'), getAllNumAndSpecCharIndex(matrix).join('\n'))
+// fs.writeFileSync(path.join(__dirname, 'paddedMatrix.txt'), paddedMatrix.join('\n'))
 
-// console.log(checkAdjacentSpaces(getAllNumAndSpecCharIndex(matrix)).join('\n'));
-// checkAdjacentSpaces(getAllNumAndSpecCharIndex(matrix))
+checkAdjacent(matrix)
+
